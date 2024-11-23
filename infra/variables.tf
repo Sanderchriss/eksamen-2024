e202c4514d3f1cee014655824a82dbcc965288b0
Sanderchriss <sanderchriss@gmail.com>
2024-11-23 11:56:27 +0000

    navnebytte

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,3 +1,9 @@
 variable "region" {
   default = "eu-west-1"
 }
+
+variable "notification_email" {
+  description = "E-postadressen som skal motta varsler"
+  type        = string
+  default     = "sado006@student.kristiania.no"
+}
